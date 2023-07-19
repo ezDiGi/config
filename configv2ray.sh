dfbb258fc7a164d2457610361ff0105ede235c18
ezDiGiDigital <104407742+ezDiGi@users.noreply.github.com>
2023-07-19 17:05:46 +0700

    configv2ray.sh

--- configv2ray.sh
+++ configv2ray.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-read -p "  ID nút (Node_ID):" node_id
-  [ -z "${node_id}" ] && node_id=0
-  echo "-------------------------------"
-  echo -e "  Node_ID: ${node_id}"
-  echo "-------------------------------"
-
-      wget https://raw.githubusercontent.com/ezDiGi/config/main/config.yml -O /etc/XrayR/config.yml
-      sed -i "s|NodeID:.*|NodeID: ${node_id}|" /etc/XrayR/aiko.yml
+wget https://raw.githubusercontent.com/ezDiGi/config/main/config.yml -O /etc/XrayR/config.yml
